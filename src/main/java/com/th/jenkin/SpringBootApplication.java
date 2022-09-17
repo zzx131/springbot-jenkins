604ea5a3c176e082a700c95f83ddb89d103388c1
zhangzexin <Aa123456>
2022-09-17 13:45:07 +0800

    去除多余的配置

--- src/main/java/com/th/jenkin/SpringBootApplication.java
+++ src/main/java/com/th/jenkin/SpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration //开启自动配置
+@EnableAutoConfiguration
 public class SpringBootApplication {
 
     public static void main(String[] args) {
