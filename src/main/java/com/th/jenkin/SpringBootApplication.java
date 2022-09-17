ba42ccfdefe8596a5d9bc91fb68b7482bb6e3b44
zhangzexin <Aa123456>
2022-09-17 13:13:39 +0800

    springboot-jenkins

---
+++ src/main/java/com/th/jenkin/SpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.th.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration //开启自动配置
+public class SpringBootApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
